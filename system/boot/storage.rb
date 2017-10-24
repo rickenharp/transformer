016ba9e0f28c7bc2f348643bf9f026be5b3cc6ea
Alexander Graefe <alex@codemo.de>
2017-10-24 10:08:56 +0200

    Fix storage implementation

--- system/boot/storage.rb
+++ system/boot/storage.rb
@@ -6,7 +6,7 @@ Application.boot(:storage) do |container|
     container.register('storage') do
       class Storage
         def download(filename)
-          JSON.dump({foo: filename})
+          JSON.dump({foo: filename, bar: 'quux'})
         end
       end
       Storage.new
